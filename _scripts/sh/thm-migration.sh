2cba0ed461e17250e659262655efe7607d7b0ac3
amir0r <amir.barnat@edu.esiee.fr>
2021-08-10 19:04:07 +0200

    publish Brainstorm WU

--- _scripts/sh/thm-migration.sh
+++ _scripts/sh/thm-migration.sh
@@ -18,7 +18,7 @@ if [ ! -z $VM ] && [ ! -z $OS ] && [ ! -z $OS ]; then
     echo "tags: [thm-$OS, writeup, oscp-prep]" >> $wu_file;
     echo "image: img/$VM.png" >> $wu_file;
     echo "---" >> $wu_file;
-    tail --lines=+4 $THM/$VM/README.md >> $wu_file;
+    tail --lines=+2 $THM/$VM/README.md >> $wu_file;
     sed -i "s/img/\/assets\/img\/thm\/$OS\/$LILVM/g" $wu_file;
 else
     echo "Usage: bash thm-migration.sh <VM_name> <OS:linux|windows|...>"
