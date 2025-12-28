f8a42f8b0c74de213e11f1a49c543624000605ae
mimiron <amir.barnat@edu.esiee.fr>
2025-12-28 13:37:58 +0100

    ci: relax htmlproofer checks for chirpy blog content

--- tools/test.sh
+++ tools/test.sh
@@ -61,9 +61,14 @@ main() {
     -d "$SITE_DIR$_baseurl" -c "$_config"
 
   # test
-  bundle exec htmlproofer "$SITE_DIR" \
+  bundle exec htmlproofer _site \
     --disable-external \
-    --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
+    --checks "Links,Scripts" \
+    --enforce-https false \
+    --ignore-urls "/^http:\/\/127\.0\.0\.1/,/^http:\/\/0\.0\.0\.0/,/^http:\/\/localhost/,/^http:\/\/(10\.|192\.168\.|172\.(1[6-9]|2[0-9]|3[0-1])\.)/,cdn\.jsdelivr\.net/,^\/tags\//,^\/categories\//"
+  #bundle exec htmlproofer "$SITE_DIR" \
+  #  --disable-external \
+  #  --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
 }
 
 while (($#)); do
